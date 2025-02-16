e809935ba4516a0282db6e03e21db4fc7f6960c2
Bohdan Lushchyk <me@forevka.dev>
2025-02-16 16:13:54 +0200

    Create SharedHostingConfiguration.cs

---
+++ Blog/Composers/SharedHostingConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.DataProtection.AuthenticatedEncryption.ConfigurationModel;
+using Microsoft.AspNetCore.DataProtection.AuthenticatedEncryption;
+using Microsoft.AspNetCore.DataProtection;
+using Umbraco.Cms.Core.Composing;
+
+namespace Blog.Composers;
+
+/// <summary>
+/// this would be loaded via assembly exploration feature
+/// </summary>
+public class SharedHostingConfiguration : IComposer
+{
+    public void Compose(IUmbracoBuilder builder)
+    {
+        var path = "/keys";
+        builder.Services
+            .AddDataProtection()
+            .PersistKeysToFileSystem(new DirectoryInfo(path))
+            .UseCryptographicAlgorithms(new AuthenticatedEncryptorConfiguration
+            {
+                EncryptionAlgorithm = EncryptionAlgorithm.AES_256_CBC,
+                ValidationAlgorithm = ValidationAlgorithm.HMACSHA256,
+            });
+    }
+}
